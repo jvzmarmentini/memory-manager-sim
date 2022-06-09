a0f71496282486c70defbae1cb0b217f3204cea4
Joao Victor Zucco Marmentini <joao.victor.zucco.marmentini@hp.com>
2022-06-09 11:19:40 -0300

    code clanup

--- Variable.py
+++ Variable.py
@@ -2,6 +2,11 @@ from Process import Process
 from utils.Colors import bc
 
 
+class MemoryOverflowException(Exception):
+    def __init__(self):
+        super().__init__(f"{bc.FAIL}!!Espaco insuficiente de memoria{bc.ENDC}")
+
+
 class Node:
     def __init__(self, data: Process):
         self.data = data
@@ -14,7 +19,7 @@ class Node:
     def __eq__(self, other: int) -> bool:
         return self.data.pid == other
 
-    def isNone(self) -> bool:
+    def isHole(self) -> bool:
         return self.data.pid is None
 
 
@@ -29,7 +34,7 @@ class DoublyLinkedList:
         while node is not None:
             nodes.append(str(node))
             node = node.next
-        return "<=>".join(nodes)
+        return "<->".join(nodes)
 
     def __iter__(self):
         node = self.head
@@ -39,10 +44,10 @@ class DoublyLinkedList:
 
     def add(self, p: Process) -> None:
         new = Node(p)
-        prev = self.head
+        cprev = self.head
 
         for cur in self:
-            if cur.data.pid is None:
+            if cur.isHole():
                 if new.data.size < cur.data.size:
                     cur.data.size -= new.data.size
                     new.prev = cur.prev
@@ -51,48 +56,56 @@ class DoublyLinkedList:
                     if cur is self.head:
                         self.head = new
                     else:
-                        prev.next = new
-                    return print(self)
+                        cprev.next = new
+                    return self
                 elif new.data.size == cur.data.size:
                     new.next = cur.next
                     new.prev = cur.prev
-                    cur.next.prev = new
+                    if cur is self.tail:
+                        self.tail = new
+                    else:
+                        cur.next.prev = new
                     if cur is self.head:
                         self.head = new
                     else:
-                        prev.next = new
-                    return print(self)
-            prev = cur
+                        cprev.next = new
+                    return self
+            cprev = cur
 
-        return print(f"{bc.FAIL}!!Espaco insuficiente de memoria{bc.ENDC}")
+        raise MemoryOverflowException
+
+    def __merge_neighbours(self, node: Node):
+        while True:
+            nprev = node.prev
+            nnext = node.next
+            if node is not self.head and nprev.isHole():
+                if(node is not self.tail):
+                    nprev.next = nnext
+                    nnext.prev = nprev
+                else:
+                    self.tail = nprev
+                    nprev.next = None
+                nprev.data.size += node.data.size
+                node = nprev
+                continue
+            if node is not self.tail and nnext.isHole():
+                if(node is not self.head):
+                    nprev.next = nnext
+                    nnext.prev = nprev
+                else:
+                    self.head = nnext
+                    nnext.prev = None
+                nnext.data.size += node.data.size
+                node = nnext
+                continue
+            return self
 
     def remove(self, n: int) -> None:
         for node in self:
             if node.data.pid == n:
                 node.data.pid = None
-                while True:
-                    if node is not self.head and node.prev.isNone():
-                        if(node is not self.tail):
-                            node.prev.next = node.next
-                            node.next.prev = node.prev
-                        else:
-                            self.tail = node.prev
-                            node.prev.next = None
-                        node.prev.data.size += node.data.size
-                        node = node.prev
-                        continue
-                    if node is not self.tail and node.next.isNone():
-                        if(node is not self.head):
-                            node.prev.next = node.next
-                            node.next.prev = node.prev
-                        else:
-                            self.head = node.next
-                            node.next.prev = None
-                        node.next.data.size += node.data.size
-                        node = node.next
-                        continue
-                    break
-                return print(self)
+                return self.__merge_neighbours(node)
+        raise ValueError(f"{bc.FAIL}!!PID nao encontrado{bc.ENDC}")
 
 
 class Variable:
@@ -103,13 +116,14 @@ class Variable:
     def __str__(self):
         return self.mem
 
-    def __in(self, p: Process) -> None:
+    def __in(self, pid: int, size: int) -> None:
+        p = Process(pid, size)
         print(f"IN: {p}")
-        self.mem.add(p)
+        return self.mem.add(p)
 
     def __out(self, pid: int) -> None:
         print(f"OUT: {pid}")
-        self.mem.remove(pid)
+        return self.mem.remove(pid)
 
     def run(self, path: str) -> None:
         with open(path) as f:
@@ -118,6 +132,13 @@ class Variable:
                 if("IN" in line):
                     pid, size = line.split("IN(", 1)[1].split(")")[
                         0].split(",")
-                    self.__in(Process(pid, size))
+                    try:
+                        print(self.__in(pid, size))
+                    except MemoryOverflowException as e:
+                        print(e)
                 else:
-                    self.__out(line.split("OUT(", 1)[1].split(")")[0])
+                    pid = line.split("OUT(", 1)[1].split(")")[0]
+                    try:
+                        print(self.__out(pid))
+                    except ValueError as e:
+                        print(e)
